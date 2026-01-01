b77140c143953e29e94f6f8a9062ceb29a989e99
Sobo Martin Iulian <35849925+Iulian85@users.noreply.github.com>
2026-01-01 16:13:57 +0100

    server port commit

--- railway-backend/server.js
+++ railway-backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = 8080
+const port = 3000
 const { Pool } = require('pg');
 const crypto = require('crypto');
 require('dotenv').config();
@@ -674,7 +674,7 @@ async function startServer() {
     await runMigrations();
     
     // 3. Pornește serverul
-    const PORT = process.env.PORT || 8080;
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, '0.0.0.0', () => {
       console.log(`✅ Server running on http://0.0.0.0:${PORT}`);
       console.log(`📡 Local: http://localhost:${PORT}`);
